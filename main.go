7ad18953f731af260e4cd10eb1594a50d60bf7e8
ivasilyev <u0412u0418u042e@gmail.com>
2023-10-09 01:52:32 +0300

    Added server routines

--- main.go
+++ main.go
@@ -1,27 +1,32 @@
-package json_to_webpage
+package main
 
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type Data struct {
-	Name  string `json:"name"`
-	Value string `json:"value"`
-}
+const PORT = 12000
 
-func main() {
-	http.HandleFunc("/", jsonDataHandler)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func templateRenderer(w http.ResponseWriter, m map[string]string) {
+	tmpl, err := template.ParseFiles("./html/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, m)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func jsonDataHandler(w http.ResponseWriter, r *http.Request) {
 	// Read the JSON file from disk
-	filePath := "data.json"
+	filePath := "./data.json"
 	fileData, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		http.Error(w, "Failed to read JSON file", http.StatusInternalServerError)
@@ -29,37 +34,35 @@ func jsonDataHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Parse the JSON data
-	var jsonData []Data
-	err = json.Unmarshal(fileData, &jsonData)
+	jsonRawData := make(map[string]json.RawMessage)
+	jsonStringData := make(map[string]string)
+
+	err = json.Unmarshal(fileData, &jsonRawData)
 	if err != nil {
-		http.Error(w, "Failed to parse JSON data", http.StatusInternalServerError)
-		return
+		panic(err)
+	}
+
+	for k, v := range jsonRawData {
+		o, _ := json.Marshal(v)
+		jsonStringData[k] = string(o)
+	}
+
+	for k, v := range jsonStringData {
+		fmt.Println("%s, %s", k, v)
 	}
 
 	// Generate HTML table
-	html := `
- <html>
- <head>
-  <link rel="stylesheet" type="text/css" href="/static/style.css">
-  <script src="/static/script.js"></script>
- </head>
- <body>
-  <table>
-   <tr>
-    <th>Name</th>
-    <th>Value</th>
-   </tr>
- `
-	for _, data := range jsonData {
-		html += fmt.Sprintf("<tr><td>%s</td><td>%s</td></tr>", data.Name, data.Value)
-	}
-	html += `
-  </table>
- </body>
- </html>
- `
+	templateRenderer(w, jsonStringData)
 
-	// Write the HTML response
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, html)
+}
+
+func main() {
+	http.HandleFunc("/", jsonDataHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
+
+	log.Println(fmt.Sprintf("Server started on http://localhost:%d", PORT))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(": %d", PORT), nil))
 }
